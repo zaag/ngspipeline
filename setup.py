a565f0c365999422951aca5a6af2a6f9047da7ef
Martin Haagmans <mahaagmans@gmail.com>
2018-10-08 13:50:15 +0200

    Added manifest and added versions to requirements

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='NGSPIPE',
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='ngspipeline',
       version='1.0',
       description='Pipeline for NGS data-analysis',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
       author='Martin Haagmans',
       author_email='mahaagmans@gmail.com',
-      url='www.github.com/zaag/ngspipeline',
+      packages=['ngspipeline'],
+      url='https://www.github.com/zaag/ngspipeline',
       license='MIT',
       scripts=['NGSPIPE'],
       packages=['ngspipeline'],
-      
-      package_data={'': ['Snakefile',  '*.rules']}
+      install_requires=['ngsscriptlibrary>=1.0', 
+                        'pycnv>=1.0']
       )
