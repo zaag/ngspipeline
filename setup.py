42f3610c1cd889706195c85b0d542092abd3113b
Martin Haagmans <mahaagmans@gmail.com>
2018-10-08 14:05:13 +0200

    New way to start  pipeline from command line

--- setup.py
+++ setup.py
@@ -7,18 +7,16 @@ def readme():
         return f.read()
 
 
-setup(name='ngspipeline',
+setup(name='NGSPIPE',
       version='1.0',
       description='Pipeline for NGS data-analysis',
       long_description=readme(),
       long_description_content_type='text/markdown',
       author='Martin Haagmans',
       author_email='mahaagmans@gmail.com',
-      packages=['ngspipeline'],
       url='https://www.github.com/zaag/ngspipeline',
       license='MIT',
       scripts=['NGSPIPE'],
-      packages=['ngspipeline'],
       install_requires=['ngsscriptlibrary>=1.0', 
                         'pycnv>=1.0']
       )
